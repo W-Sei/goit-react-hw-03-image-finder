ca488a38b8dfe5b68c1593c493366ed5e25d9379
Hanna Franko <sei.wrcl@gmail.com>
2023-03-10 04:23:08 +0200

    add global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyles } from 'GlobalStyles.styled';
+import { Wrapper } from './Wrapper/Wrapper.styled';
+
+export class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <GlobalStyles />
+        <div>React homework template</div>
+      </Wrapper>
+    );
+  }
+}
---
+++ src/components/Wrapper/Wrapper.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+  /* align-items: center; */
+  background-color: black;
+  color: white;
+  /* max-width: 700px; */
+  margin: 0 auto;
+
+`;
