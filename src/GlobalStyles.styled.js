5a2dbc779da187c94be9ac712640f449b9c6f9db
Hanna Franko <sei.wrcl@gmail.com>
2023-03-11 02:15:55 +0200

    fix bgcolor

--- src/GlobalStyles.styled.js
+++ src/GlobalStyles.styled.js
@@ -10,7 +10,7 @@ body {
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background-color: #e7e7e2;
+  background-color: white;
 }
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
